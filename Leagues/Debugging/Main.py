8b87d204e0fc5a402d0d6d707795f5db6a106352
sh5sui <leafeyshadow@gmail.com>
2025-11-23 14:29:10 +0000

    Added small changes to free agency command

--- Leagues/Debugging/Main.py
+++ Leagues/Debugging/Main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import logging
+from discord import app_commands
 
 load_dotenv()
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -19,8 +20,6 @@ freeagentchannel_id = 1387870667411558561
 friendlieschannel_id = 1387782160697655409
 contractschannel_id = 1387870804225687764
 
-freeagentchannel = bot.get_channel(freeagentchannel_id)
-
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -33,12 +32,18 @@ async def serverinfo(interaction: discord.Interaction):
 
 @bot.tree.command(name="links")
 async def links(interaction: discord.Interaction):
-    await interaction.response.send_message("*Discord*: https://discord.gg/ufa-united-football-association-836277409245298708\n*Pitch*: Under Development\n*Others*: Coming soon")
+    await interaction.response.send_message("*Discord*: https://discord.gg/ufa-united-football-association-836277409245298708\n*Pitch*: https://www.roblox.com/games/137624913542642\n*Others*: Coming soon")
 
 @bot.tree.command(name="freeagency")
-async def freeagency(interaction: discord.Interaction):
+@app_commands.describe(
+    position="Your position",
+    region="Your region"
+)
+async def freeagency(interaction: discord.Interaction, position: str, region: str):
+    freeagentchannel = bot.get_channel(1387870667411558561)
     user_id = interaction.user.id
-    await freeagentchannel.send(f"<@{user_id}> is a free agent!")
+    await interaction.response.send_message("Free agency post sent!", ephemeral=True)
+    await freeagentchannel.send(f"<@{user_id}> is a free agent! Position: {position}, Region: {region}")
 
 @bot.event
 async def on_message(message):
