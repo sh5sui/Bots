034351f01b91f66259c3d390549b2ceb0bd78476
sh5sui <leafeyshadow@gmail.com>
2025-11-23 13:56:53 +0000

    Added intents + Logging handler

--- Leagues/Debugging/Main.py
+++ Leagues/Debugging/Main.py
@@ -1,11 +1,16 @@
 import discord
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
+intents.typing = False
 intents.message_content = True
+intents.members = True
+intents.presences = True
 
 client = discord.Client(intents=intents)
 
@@ -21,5 +26,5 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-Token = os.getenv("Token")
+Token = os.getenv("Token", log_Handler=handler)
 client.run(Token)
