a81d212bd95a60a72cc8f122ab43114dd1b42085
sh5sui <leafeyshadow@gmail.com>
2025-11-23 13:21:30 +0000

    Base

--- Leagues/Debugging/Main.py
+++ Leagues/Debugging/Main.py
@@ -1,1 +1,14 @@
+import discord
 
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Online as {self.user}')
+
+        async def on_message(self, message):
+            print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('Token')
