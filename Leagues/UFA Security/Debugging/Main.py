7f17a70a6cb0bf1f899fe83c25a3d43b612f6536
sh5sui <leafeyshadow@gmail.com>
2025-11-24 02:36:05 +0000

    Created security bot env variables and other things. Added first two commands to security bot. Ban and kick.

--- Leagues/UFA Security/Debugging/Main.py
+++ Leagues/UFA Security/Debugging/Main.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -16,5 +19,42 @@ async def on_ready():
     print(f"Connected as {bot.user}")
     await bot.tree.sync()
 
-token = os.getenv("Token")
-bot.run(token)
+@bot.tree.command(name="ban")
+async def ban(interaction: discord.Interaction, member: discord.Member = None, reason: str = "No Reason"):
+    if not interaction.user.guild_permissions.ban_members:
+        await interaction.response.send_message("You don't have permission to run this command.", ephemeral=True)
+        return
+    
+    if member == interaction.user:
+        await interaction.response.send_message("You cannot ban yourself.", ephemeral=True)
+        return
+    
+    if member.top_role >= interaction.user.top_role and interaction.user != interaction.guild.owner:
+        await interaction.response.send_message("You cannot ban someone with a higher role than you.", ephemeral=True)
+        return
+    
+    logschannel = bot.get_channel = 123
+    await member.ban(reason=reason)
+    await interaction.response.send_message(f"{member} Was banned for {reason}")
+    await logschannel.send(f"Ban command was executed by {interaction.user} on {member}")
+
+@bot.tree.command(name="kick")
+async def kick(interaction: discord.Interaction, member: discord.Member = None, reason: str = "No Reason"):
+    if not interaction.user.guild_permissions.kick_members:
+        await interaction.response.send_message("You don't have permission to run this command.", ephemeral=True)
+        return
+    
+    if member == interaction.user:
+        await interaction.response.send_message("You cannot kick yourself.", ephemeral=True)
+        return
+    
+    if member.top_role >= interaction.user.top_role and interaction.user != interaction.guild.owner:
+        await interaction.response.send_message("You cannot kick someone with a higher role than you.", ephemeral=True)
+
+        logschannel = bot.get_channel = 123
+        await member.kick(reason=reason)
+        await interaction.response.send_message(f"{member} Was kicked for {reason}")
+        await logschannel.send(f"Kick command was executed by {interaction.user} on {member}")
+
+Token = os.getenv("Token")
+bot.run(Token)
